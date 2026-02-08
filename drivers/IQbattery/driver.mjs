88efa4dec36feb6e176f7f553f3c2e5c338bcc2e
Wouwmw <118289604+Wouwmw@users.noreply.github.com>
2026-02-08 20:23:51 +0100

    Rename class to EnphaseDriverIQBattery

--- drivers/IQbattery/driver.mjs
+++ drivers/IQbattery/driver.mjs
@@ -1,5 +1,5 @@
 import EnphaseDriver from '../../lib/iqbattery/iqbattery_EnphaseDriver.mjs';
 
-export default class EnphaseDriverInverter extends EnphaseDriver {
+export default class EnphaseDriverIQBattery extends EnphaseDriver {
 
 };
